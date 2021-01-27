fa108d40463f3097c169024f2f3b641c93fc271b
dwipalshrirao <dwipalshrirao456@gmail.com>
2021-01-27 17:09:55 +0530

    whitenoise configuration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -44,7 +44,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,7 +132,7 @@ STATIC_URL = '/static/'
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT=[os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT=[os.path.join(BASE_DIR, 'staticfiles')]
 
 
 
