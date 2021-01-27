d11c9fd82f09a5add22263887ae175fbeb99cd18
dwipalshrirao <dwipalshrirao456@gmail.com>
2021-01-27 23:26:29 +0530

    update staicfiles

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),os.path.join(BASE_DIR, 'media')]
 
 
 
